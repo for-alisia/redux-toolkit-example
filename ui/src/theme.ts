bc27c408a09cc74b5940ab3dae9bf68883e8984d
Elena <for-alisia@yandex.ru>
2024-01-29 15:08:15 +0100

    Created MeetingsCard

--- ui/src/theme.ts
+++ ui/src/theme.ts
@@ -4,9 +4,9 @@ const customTheme = {
     colorSuccess: '#264E36',
     colorTextBase: '#FFF2EC',
     colorWarning: '#BA3242',
+    colorLink: '#B89B72',
     fontFamily: 'Source Sans 3',
     fontSize: 16,
-    colorLink: '#B89B72',
   },
 };
 
